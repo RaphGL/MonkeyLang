c7ad4d953128200b1f15c129290010d124d886a3
RaphGL <raphfl.dev@gmail.com>
2024-07-09 19:31:41 -0100

    parsed prefix expressions

--- parser.rs
+++ parser.rs
@@ -1,15 +1,33 @@
 use crate::lexer::{Lexer, Token};
 
+#[derive(Debug)]
+enum Expression {
+    Int(i64),
+    Ident(String),
+    PrefixNeg(Box<Expression>),
+    PrefixNot(Box<Expression>),
+}
+
 pub enum Statement {
     Let {
         name: Token,
         // value: Expression
     },
-
     Return, // (value: Expr);
+    ExpressionStmt(Expression),
 }
 
-type Program = Vec<Statement>;
+enum Precedence {
+    Lowest = 1,
+    Equals,      // ==
+    LessGreater, // > or <
+    Sum,         // +
+    Product,     // *
+    Prefix,      // -X or !X
+    Call,        // myFunction(X)
+}
+
+type Program<'a> = Vec<Statement>;
 
 pub struct Parser<'a> {
     lexer: Lexer<'a>,
@@ -83,8 +101,63 @@ impl<'a> Parser<'a> {
         match self.curr_token {
             Token::Let => self.parse_let_statement(),
             Token::Return => self.parse_return_statement(),
-            _ => None,
+            _ => self.parse_expression_statement(),
         }
+    }
+
+    fn parse_prefix(&mut self) -> Option<Statement> {
+        let stmt = match self.curr_token {
+            Token::Ident(ref ident) => Statement::ExpressionStmt(Expression::Ident(ident.clone())),
+            Token::Int(ref int) => {
+                let Ok(int) = int.parse::<i64>() else {
+                    return None;
+                };
+
+                Statement::ExpressionStmt(Expression::Int(int))
+            }
+
+            Token::Bang | Token::Minus => {
+                let token = self.curr_token.clone();
+                self.next_token();
+                let Some(Statement::ExpressionStmt(expr)) =
+                    self.parse_expression(Precedence::Prefix)
+                else {
+                    return None;
+                };
+
+                Statement::ExpressionStmt(match token {
+                    Token::Bang => Expression::PrefixNot(expr.into()),
+                    Token::Minus => Expression::PrefixNeg(expr.into()),
+                    _ => unreachable!(),
+                })
+            }
+
+            ref token @ _ => {
+                self.errors
+                    .push(format!("no prefix parsing available for {token:?}"));
+                return None;
+            }
+        };
+
+        Some(stmt)
+    }
+    fn parse_infix(&mut self) {
+        todo!()
+    }
+
+    fn parse_expression(&mut self, precedence: Precedence) -> Option<Statement> {
+        let prefix = self.parse_prefix();
+        prefix
+    }
+
+    fn parse_expression_statement(&mut self) -> Option<Statement> {
+        let expr = self.parse_expression(Precedence::Lowest)?;
+
+        if self.next_token == Token::Semicolon {
+            self.next_token();
+        }
+
+        Some(expr)
     }
 
     pub fn parse(&mut self) -> Option<Program> {
@@ -114,6 +187,14 @@ mod tests {
         }
     }
 
+    fn new_program(input: &str) -> Option<Program> {
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+        let program = parser.parse();
+        check_parser_errors(&parser);
+        program
+    }
+
     #[test]
     fn let_statements() {
         let input = r#"
@@ -122,12 +203,7 @@ mod tests {
            let foobar = 838383; 
         "#;
 
-        let lexer = Lexer::new(input);
-        let mut parser = Parser::new(lexer);
-        let program = parser.parse();
-        check_parser_errors(&parser);
-
-        let program = program.unwrap();
+        let program = new_program(input).unwrap();
         if program.len() != 3 {
             panic!();
         }
@@ -152,17 +228,108 @@ mod tests {
             return 993322;
         "#;
 
-        let lexer = Lexer::new(input);
-        let mut parser = Parser::new(lexer);
-        let program = parser.parse();
-        check_parser_errors(&parser);
-
-        let program = program.unwrap();
+        let program = new_program(input).unwrap();
+        if program.len() != 3 {
+            panic!();
+        }
 
         for stmt in program {
             let Statement::Return = stmt else {
                 panic!();
             };
+        }
+    }
+
+    #[test]
+    fn identifier_expression() {
+        let input = "foobar";
+
+        let program = new_program(input).unwrap();
+        if program.len() != 1 {
+            panic!();
+        }
+
+        let Statement::ExpressionStmt(token) = &program[0] else {
+            panic!()
+        };
+
+        let Expression::Ident(name) = token else {
+            panic!();
+        };
+
+        assert_eq!(name, input);
+    }
+
+    #[test]
+    fn integer_literal_expression() {
+        let input = "5";
+
+        let program = new_program(input).unwrap();
+        if program.len() != 1 {
+            panic!();
+        }
+
+        let Statement::ExpressionStmt(token) = &program[0] else {
+            panic!()
+        };
+
+        let Expression::Int(name) = *token else {
+            panic!();
+        };
+
+        assert_eq!(name, input.parse::<i64>().unwrap());
+    }
+
+    #[test]
+    fn prefix_expressions() {
+        struct Prefix<'a> {
+            input: &'a str,
+            operator: &'a str,
+            int_val: i64,
+        }
+
+        let prefixes = [
+            Prefix {
+                input: "!5",
+                operator: "!",
+                int_val: 5,
+            },
+            Prefix {
+                input: "-15",
+                operator: "-",
+                int_val: 15,
+            },
+        ];
+
+        for prefix in prefixes {
+            let program = new_program(prefix.input).unwrap();
+
+            if program.len() != 1 {
+                panic!();
+            }
+
+            let Statement::ExpressionStmt(ref token) = program[0] else {
+                panic!();
+            };
+
+            let expr = match token {
+                Expression::PrefixNeg(expr) => {
+                    assert_eq!(prefix.operator, "-");
+                    expr
+                }
+                Expression::PrefixNot(expr) => {
+                    assert_eq!(prefix.operator, "!");
+                    expr
+                }
+
+                _ => panic!(),
+            };
+
+            if let Expression::Int(int) = **expr {
+                assert_eq!(prefix.int_val, int);
+            } else {
+                panic!();
+            }
         }
     }
 }
